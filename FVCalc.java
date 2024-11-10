d96cc54de77fd56036c391b06276b937957f3022
IdoLahav <ido.lahav@post.runi.ac.il>
2024-11-10 14:43:07 +0200

    initial draft for hw

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);		
 		int years = Integer.parseInt(args[2]);	
-		double futureValue_temp = currentValue * Math.pow(rate/100 + 1,years);
+		double futureValue_temp = currentValue * Math.pow(rate/100.0 + 1,years);
 		int futureValue = (int)futureValue_temp;
-		System.out.println("AAfter " + years + " years, " + currentValue + "$ saved at " + rate + "%" + " will yield $" + futureValue);
+		System.out.println("After " + years + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + futureValue);
 	}
 }
